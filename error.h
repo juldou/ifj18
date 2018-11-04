0c1ec5bae86c1cf46a876e75437bcabb961b4489
juldou <xmarko17@stud.fit.vutbr.cz>
2018-11-04 13:26:19 +0100

    error handling codes

---
+++ error.h
@@ -0,0 +1,13 @@
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+#define ERR_LEXICAL 1
+#define ERR_SYNTAX 2
+#define ERR_SEMANTIC_DEFINITION 3
+#define ERR_SEMANTIC_TYPE 4
+#define ERR_SEMANTIC_PARAMETERS_COUNT 5
+#define ERR_SEMANTIC_OTHER 6
+#define ERR_ZERO_DIVISION 9
+#define ERR_INTERNAL 99
+
+#endif
