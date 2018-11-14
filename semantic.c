18d945c692b56b08c8b7f11e268dc6bfd68a131a
juldou <xmarko17@stud.fit.vutbr.cz>
2018-11-14 16:48:39 +0100

    fix PR from comments

--- semantic.c
+++ semantic.c
@@ -21,7 +21,7 @@ int insert_fun_to_st(char *fun_id) {
 
     init_fun_elem_data(data);
 
-    data->id = (char *) malloc((strlen(fun_id) + 1) * sizeof(char *));
+    data->id = (char *) malloc((strlen(fun_id) + 1) * sizeof(char));
     if (data->id == NULL) return ERR_INTERNAL;
     strcpy(data->id, fun_id);
 
@@ -38,10 +38,10 @@ int semantic_check_fun_defined(char *fun_id) {
 /* add parameter to function declaration in symtable */
 int semantic_add_fun_param(char *fun_id, char *param) {
     st_elem *elem = st_search(&st_global, fun_id);
-    elem->data->params = (char **) realloc(elem->data->params, sizeof(char **));
+    elem->data->params = (char **) realloc(elem->data->params, sizeof(*(elem->data->params)) + sizeof(char *));;
     if (elem->data->params == NULL) return ERR_INTERNAL;
 
-    elem->data->params[elem->data->params_count] = (char *) malloc((strlen(param) + 1) * sizeof(char *));
+    elem->data->params[elem->data->params_count] = (char *) malloc((strlen(param) + 1) * sizeof(char));
     if (elem->data->params[elem->data->params_count] == NULL)
         return ERR_INTERNAL;
     else {
